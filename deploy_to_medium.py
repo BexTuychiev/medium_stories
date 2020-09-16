14362a05bd4fe39ca74172c80a1c21c8b69605dd
Bex Tuychiev <bex@ibexprogramming.com>
2020-09-16 16:52:53 +0500

    Deploy the first draft

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 
 jtm.publish('merge_data_article/main.ipynb',
-            integration_token="222f6677e2c8d29a810fc29db5f70b51086db498986340647485c9ba6c3f345dc",
+            integration_token="",
             pub_name=None,
             title=None,
             tags=None,

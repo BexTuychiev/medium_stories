7783575f2f04bf297a3868d603943776a58c33c2
Bex Tuychiev <bex@ibexprogramming.com>
2020-10-08 22:48:24 +0500

    Final draft save and deploy to medium

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('',
+jtm.publish('hardest_of_pandas/hardest_of_pandas.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

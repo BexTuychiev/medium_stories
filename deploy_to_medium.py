e7a0672e068af7c24c8e0801e11b7a8ba2c1e1b3
Bex Tuychiev <bex@ibexprogramming.com>
2020-10-09 15:35:44 +0500

    Setup for the second part of hardest pandas functions

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('hardest_of_pandas/hardest_of_pandas.ipynb',
+jtm.publish('',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

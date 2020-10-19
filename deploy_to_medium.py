65ef1413758051a1d3ab73c30e39b463fb1279a7
Bex Tuychiev <bex@ibexprogramming.com>
2020-10-19 10:32:13 +0500

    Prepare setup for cut_qcut article

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('hardest_of_pandas3/hardest_of_pandas3.ipynb',
+jtm.publish('',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

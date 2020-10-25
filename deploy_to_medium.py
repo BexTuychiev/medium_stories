439c54aed2104f104d7ca337876a205a96b2e2cc
Bex Tuychiev <bex@ibexprogramming.com>
2020-10-25 15:56:26 +0500

    Add the setup for categorical_data_cleaning

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('common_data_problems/common_data_problems.ipynb',
+jtm.publish('',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

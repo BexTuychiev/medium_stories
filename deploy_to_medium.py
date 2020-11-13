4c29085c1f82cc0f623e924d50e87ff998db4294
Bex Tuychiev <bex@ibexprogramming.com>
2020-11-13 20:16:39 +0500

    deploy: Deployed FuzzyWuzzy article to Medium:

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('',
+jtm.publish('november_2020/string_matching_w_fuzzy_wuzzy/fuzzy_wuzzy.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

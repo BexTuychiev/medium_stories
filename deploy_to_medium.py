7241232c93c14249d8f0abb99b44bb731dcae317
Bex Tuychiev <bex@ibexprogramming.com>
2020-11-17 13:13:07 +0500

    article: Article 24, Recordlinkage Package

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('november_2020/visualizing_missingno/new_package_in_town.ipynb',
+jtm.publish('',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

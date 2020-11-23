c78a2f8ad918dcc8fb749ac0920174b71ccc3c71
Bex Tuychiev <bex@ibexprogramming.com>
2020-11-23 16:05:50 +0500

    section: What is Streamlit, Article 27

--- november_2020/climate_streamlit/sample.py
+++ november_2020/climate_streamlit/sample.py
@@ -4,21 +4,19 @@ import streamlit as st
 from PIL import Image
 
 # Subhead
-st.subheader('Wait, what was Streamlit again?')
+st.header('Wait, what was Streamlit again?')
 # Load images using PIL
 image = Image.open('images/streamlit.png')
 # Render images using st.image() with caption
-st.image(image, caption='Streamlit homepage. Image by author.')
+st.image(image, caption='Streamlit homepage. Image by author.', use_column_width=True)
 st.markdown("""
-    <p>
     Even though developed in 2013, 2019 has seen a massive surge in the usage of the framework. 
     I think the framework's main attraction is its dead simplicity and blazing fast development cycle. 
     It has been such a blessing for people in data sphere who hate static notebooks but are too 
     lazy to use web-development to deploy their models into a web-app.
 
     You can see some of the apps developed using Streamlit on 
-    <a href='https://www.streamlit.io/gallery'></a>this link.
-    </p>
+    [this]('https://www.streamlit.io/gallery') link.
 
     To create apps using `Streamlit`, you should install it via pip:
 

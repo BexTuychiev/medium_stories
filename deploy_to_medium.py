2041b08db68e0be2c9a42a946224fb663799a196
Bex Tuychiev <bex@ibexprogramming.com>
2020-11-24 23:23:50 +0500

    file: Update .gitignore once again

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('november_2020/climate_streamlit/climacell.ipynb',
+jtm.publish('',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

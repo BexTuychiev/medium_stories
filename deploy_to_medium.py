5fe6dfbc615fc163cb682e13b7d493f919832083
Bex Tuychiev <bex@ibexprogramming.com>
2020-11-26 22:33:56 +0500

    deploy: Article 29

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('',
+jtm.publish('november_2020/10_functions/functions.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

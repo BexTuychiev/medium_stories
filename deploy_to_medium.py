74c1af384a57d941957f10b9f46cc3742b626c77
Bex Tuychiev <bex@ibexprogramming.com>
2020-12-21 00:08:39 +0500

    deploy: Article 34

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('',
+jtm.publish('december_2020/3_log_scale/log_scale.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

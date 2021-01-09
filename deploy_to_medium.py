9fbe324912b27de283881a0587410d22fff85d8c
Bex Tuychiev <bex@ibexprogramming.com>
2021-01-09 11:21:05 +0500

    article: Correlation and regression

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2020/december/3_log_scale/log_scale.ipynb',
+jtm.publish('',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

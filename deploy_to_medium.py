c888f6d7a0580447bcfd079ad5762f2760f0ca32
Bex Tuychiev <bex@ibexprogramming.com>
2021-01-15 22:02:35 +0500

    deploy: Article 37, Poisson distribution, intuition

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('',
+jtm.publish('2021/january/3_poisson_distribution/poisson.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

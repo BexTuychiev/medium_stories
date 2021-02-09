d9f2c0e47660afb19ce5e27abac455f0fc9f798b
Bex Tuychiev <bex@ibexprogramming.com>
2021-02-09 22:57:56 +0500

    deploy: Article 42, ML Dictionary

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('',
+jtm.publish('2021/february/2_ml_dictionary/ml_dict.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

0148815b4b4638f434866b5a104fdbf5c9d21fa4
Bex Tuychiev <bex@ibexprogramming.com>
2021-02-15 19:57:05 +0500

    article: Data Lakes: Definitive Guide, Article 44

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/february/4_knn/knn.ipynb',
+jtm.publish('',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

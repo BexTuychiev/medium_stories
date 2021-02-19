29534145ab89ff8018c657b793e2eabff5c4c53d
Bex Tuychiev <bex@ibexprogramming.com>
2021-02-19 22:27:33 +0500

    deploy: Article 44, Train/Test split generation

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('',
+jtm.publish('2021/february/5_train_test_generation/train_test.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

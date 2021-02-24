b1dccad62bc28d1386a30843423a14742f20bdea
Bex Tuychiev <bex@ibexprogramming.com>
2021-02-24 19:18:00 +0500

    section: Setup, Article 46

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/february/6_pipelines/pipelines.ipynb',
+jtm.publish('',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

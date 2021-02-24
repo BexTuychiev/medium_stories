92e07212735a453616b27d46b8a3c5aa6b8acf89
Bex Tuychiev <bex@ibexprogramming.com>
2021-02-24 10:02:48 +0500

    deploy: Article 45, SKlearn pipelines

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('',
+jtm.publish('2021/february/6_pipelines/pipelines.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

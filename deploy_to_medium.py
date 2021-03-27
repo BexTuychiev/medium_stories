8a0ddc2a4cd6a624bc9f178b5f9d3710050fd55e
Bex Tuychiev <bex@ibexprogramming.com>
2021-03-27 15:38:44 +0500

    article: Plotply pandas backend

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/march/1_hyperparams_tuning/hyperparams.ipynb',
+jtm.publish('',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

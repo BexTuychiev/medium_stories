623592f18c669553158a1a03e6fc2f5fd3cb7566
Bex Tuychiev <bex@ibexprogramming.com>
2021-04-07 17:32:43 +0500

    section: Small roadmap connecting the sources

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/april/1_xgboost_classification/xgboost_classification.ipynb',
+jtm.publish('',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

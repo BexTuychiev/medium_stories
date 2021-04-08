c798445dad598fa1db774516dc03919c509eb05e
Bex Tuychiev <bex@ibexprogramming.com>
2021-04-08 10:26:55 +0500

    article: Article 52, Halving Grid and Random Searches

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/april/2_top_sources/top_resources.ipynb',
+jtm.publish('',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

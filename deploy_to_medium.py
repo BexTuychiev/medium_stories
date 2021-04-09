5752a5674a5d944ea797acdee6db8d4e671172cf
Bex Tuychiev <bex@ibexprogramming.com>
2021-04-09 22:24:01 +0500

    deploy: Article 52

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('',
+jtm.publish('2021/april/3_halving_searches/halving_searches.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

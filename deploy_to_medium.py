09cbb2a4cdade76f8e8f0ccf52fccbae2bb0e0d6
Bex Tuychiev <bex@ibexprogramming.com>
2021-04-10 23:20:48 +0500

    deploy: Article 53, Variance Thresholding

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('',
+jtm.publish('2021/april/4_variance_threshold/variance_threshold.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

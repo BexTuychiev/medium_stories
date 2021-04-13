d17bda58c6b6e091f760a3df32f1ace722fae3cb
Bex Tuychiev <bex@ibexprogramming.com>
2021-04-13 17:21:15 +0500

    deploy: Article 55, How to Use Pairwise Correlation For Robust Feature Selection

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('',
+jtm.publish('2021/april/6_pairwise_correlation/pairwise_correlation.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

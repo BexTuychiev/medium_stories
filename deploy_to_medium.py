4d06e37a6381758443f8a4af3ba178ea94ddba5f
Bex Tuychiev <bex@ibexprogramming.com>
2021-05-08 14:10:00 +0500

    article: Class-level vs. Instance-level data in OOP Python, Article 56

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/april/6_pairwise_correlation/pairwise_correlation.ipynb',
+jtm.publish('',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

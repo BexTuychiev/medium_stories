8ba34764db368aa9bdab9a5a25c7bf4e2e61e9bf
Bex Tuychiev <bex@ibexprogramming.com>
2021-05-11 11:40:02 +0500

    deploy: Article 57, RFE

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('',
+jtm.publish('2021/may/2_rfe/rfe.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

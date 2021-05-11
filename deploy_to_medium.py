d1270a779f908c4b2f740febce04b7bffcaa9fa7
Bex Tuychiev <bex@ibexprogramming.com>
2021-05-11 12:13:20 +0500

    article: Bad-ass feature selection by combining multiple models, Article 58

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/may/2_rfe/rfe.ipynb',
+jtm.publish('',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title=None,

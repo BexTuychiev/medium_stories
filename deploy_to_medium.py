81798a51a9dd929db77a1719d3bab5574c7b18ec
Bex Tuychiev <bex@ibexprogramming.com>
2021-06-06 14:09:17 +0500

    section: Hyperparameter tuning to optimize model performance for a custom metric

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/',
+jtm.publish('2021/june/1_multiclass_classification/multiclass.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title='',
+            title='Ultimate Guide to Multiclass Classification With Sklearn',
             tags=None,
             publish_status='draft',
             notify_followers=False,

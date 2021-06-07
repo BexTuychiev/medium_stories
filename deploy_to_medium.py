00ce02bd9a4e8a44c35b01c91ecb0ec65f6008ad
Bex Tuychiev <bex@ibexprogramming.com>
2021-06-07 13:39:41 +0500

    article: Article 67, Multiclass Classification Metircs Explained Neatly

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/june/1_multiclass_classification/multiclass.ipynb',
+jtm.publish('2021/june',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title='',

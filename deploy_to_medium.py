da83f9ce4af3b03c02c9ac152bb23f64e4c10c10
Bex Tuychiev <bex@ibexprogramming.com>
2021-06-09 18:49:34 +0500

    deploy: Article 67

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/june',
+jtm.publish('2021/june/2_multiclass_metrics/multiclass_metrics.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title='',
+            title='Comprehensive Guide on Multiclass Classification Metrics',
             tags=None,
             publish_status='draft',
             notify_followers=False,

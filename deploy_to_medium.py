149fef3a04c535c749ea4ec6b19c394d5c8b8942
Bex Tuychiev <bex@ibexprogramming.com>
2021-06-10 12:03:16 +0500

    article: Article 68, SQL fundamentals for Pandas users

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/june/2_multiclass_metrics/multiclass_metrics.ipynb',
+jtm.publish('2021/june/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title='Comprehensive Guide on Multiclass Classification Metrics',
+            title='',
             tags=None,
             publish_status='draft',
             notify_followers=False,

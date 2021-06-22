a9802ba17ac2dd42bc3e786c5eca8ef214ab64b0
Bex Tuychiev <bex@ibexprogramming.com>
2021-06-22 13:59:13 +0500

    article: Article 70, Sklearn Features You didn't know existed

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/june/6_open_source_object_storage/open_source_object_storage.ipynb',
+jtm.publish('2021/june/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title='',

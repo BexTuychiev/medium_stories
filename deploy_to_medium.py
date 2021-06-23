9292d575520f034f21efd893d14473e8098129c4
Bex Tuychiev <bex@ibexprogramming.com>
2021-06-23 18:47:42 +0500

    article: Article 71, N Super Productive Things To Do During Model Training

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/june/5_unknown_sklearn_features/unknown_sklearn_features.ipynb',
+jtm.publish('2021/june/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title='',

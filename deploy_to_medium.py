efe33ef3bc5682e0ecf0f303c488f0fa672022e9
Bex Tuychiev <bex@ibexprogramming.com>
2021-06-28 14:51:27 +0500

    section: 5. Perform feature selection or dimensionality reduction

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/june/',
+jtm.publish('2021/june/8_my_models_sucked/fix_models.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title='',

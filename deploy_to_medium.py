d30103329cff306f51ebecf253f73f35cf6b1b71
Bex Tuychiev <bex@ibexprogramming.com>
2021-07-01 15:05:35 +0500

    deploy: Article 74

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/',
+jtm.publish('2021/july/1_github_copilot/copilot.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title='',
+            title='OpenAI Launches GitHub Copilot: AI Focused On Code Generation. Should We Be Worried Now?',
             tags=None,
             publish_status='draft',
             notify_followers=False,

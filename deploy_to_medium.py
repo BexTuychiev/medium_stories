a8728dda2d02c0219e64c95a042480ed0bf14941
Bex Tuychiev <bex@ibexprogramming.com>
2021-07-01 10:49:01 +0500

    article: Article 74, GitHub Copilot, OpenAI

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/june/9_plotly_fundamentals/express.ipynb',
+jtm.publish('2021/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title='',

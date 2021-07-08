ce294e7ff6ceff80e4d03971d58bdfd7f2d3ecbe
Bex Tuychiev <bex@ibexprogramming.com>
2021-07-08 18:44:11 +0500

    section: 5. Comparing the growth of multiple time series

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/july/3_time_series_manipulation/time_series.ipynb',
+jtm.publish('2021/july/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title='',

af68cd09cb60ae996f0cde3371e5d9d69c9728bd
Bex Tuychiev <bex@ibexprogramming.com>
2021-07-09 14:46:20 +0500

    deploy: Deploy Article 76, Every Function You Can (Should) Use in Pandas to Manipulate Time Series

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/july/',
+jtm.publish('2021/july/3_time_series_manipulation/time_series.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title='',
+            title='Every Function You Can (Should) Use in Pandas to Manipulate Time Series',
             tags=None,
             publish_status='draft',
             notify_followers=False,

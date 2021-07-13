c8de1781ed474733ff44525cad3b047d1ab762af
Bex Tuychiev <bex@ibexprogramming.com>
2021-07-13 12:43:06 +0500

    deploy: Article 77

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/july/',
+jtm.publish('2021/july/4_advanced_ts_visuals/ts_visualize.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title='',
+            title='Advanced Time Series Analysis in Python: Seasonality and Trend Analysis (Decomposition), Autocorrelation',
             tags=None,
             publish_status='draft',
             notify_followers=False,

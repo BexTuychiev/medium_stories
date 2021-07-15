7b1892d5825db5bb15c6562e87988af527d976e6
Bex Tuychiev <bex@ibexprogramming.com>
2021-07-15 22:24:16 +0500

    deploy: Article 78

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/july/',
+jtm.publish('2021/july/5_walk_noise_ts/random_walk_noise.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title='',
+            title='How to Detect Random Walk and White Noise in Time Series Forecasting',
             tags=None,
             publish_status='draft',
             notify_followers=False,

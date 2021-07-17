e65a9d30b999b2a9b3bba818dd5b80e0d92a82f5
Bex Tuychiev <bex@ibexprogramming.com>
2021-07-17 19:05:03 +0500

    deploy: Article 79

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/july/',
+jtm.publish('2021/july/6_stationarity_ts/stationarity.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title='',
+            title='How to Remove Non-Stationarity in Time Series Forecasting',
             tags=None,
             publish_status='draft',
             notify_followers=False,

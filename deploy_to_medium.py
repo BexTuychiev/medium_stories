f335781c9a7810ced08294e5764798efee63bd58
Bex Tuychiev <bex@ibexprogramming.com>
2021-07-29 14:00:51 +0500

    article: Article 82, Why is Everyone at Kaggle Obsessed with Optuna

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/july/8_xgboost_faqs/faqs.ipynb',
+jtm.publish('2021/july/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title='20 Burning XGBoost FAQs Answered to Use the Library Like a Pro',
+            title='',
             tags=None,
             publish_status='draft',
             notify_followers=False,

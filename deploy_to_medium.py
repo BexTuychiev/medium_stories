36b96ceeff5981688f99f667261545c989926786
Bex Tuychiev <bex@ibexprogramming.com>
2021-08-03 12:50:47 +0500

    deploy: Article 83 to Medium

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/august/',
+jtm.publish('2021/august/1_pandas_funcs/pandas_funcs.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title='',
+            title='25 Pandas Functions You Didn\'t Know Existed | P(Guarantee) = 0.8',
             tags=None,
             publish_status='draft',
             notify_followers=False,

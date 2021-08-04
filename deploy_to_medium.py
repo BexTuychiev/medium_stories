7c7f3ae9769b2d8c9abc31b1662124b6cb5fd4dd
Bex Tuychiev <bex@ibexprogramming.com>
2021-08-04 10:07:28 +0500

    article: Article 84, N Coolest Python Packages Kagglers Are Using Without Telling You

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/august/1_pandas_funcs/pandas_funcs.ipynb',
+jtm.publish('2021/august/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title='25 Pandas Functions You Didn\'t Know Existed | P(Guarantee) = 0.8',
+            title='',
             tags=None,
             publish_status='draft',
             notify_followers=False,

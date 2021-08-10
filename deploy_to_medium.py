4cfbbb972bcf01e18f408fee21d80b78736423eb
Bex Tuychiev <bex@ibexprogramming.com>
2021-08-10 18:25:40 +0500

    deploy: Article 85

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/august/',
+jtm.publish('2021/august/3_masterpiece_matplotlib/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title='',
+            title='Yes, These Unbelievable Masterpieces Are Created With Matplotlib',
             tags=None,
             publish_status='draft',
             notify_followers=False,

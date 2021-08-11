7c81ff7576a86ae0e33bfe4cb5ed8df690b4a690
Bex Tuychiev <bex@ibexprogramming.com>
2021-08-11 12:30:05 +0500

    file: Add a python script to run manim examples

--- 2021/august/4_manim_basics/scene.py
+++ 2021/august/4_manim_basics/scene.py
@@ -1,8 +1,18 @@
 from manim import *
 
 
-class SquareToCircle(Scene):
+class MobjectPlacement(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))  # show the circle on screen
+        circle = Circle()
+        square = Square()
+        triangle = Triangle()
+
+        # place the circle two units left from the origin
+        circle.move_to(LEFT * 2)
+        # place the square to the left of the circle
+        square.next_to(circle, LEFT)
+        # align the left border of the triangle to the left border of the circle
+        triangle.align_to(circle, LEFT)
+
+        self.add(circle, square, triangle)
+        self.wait(1)

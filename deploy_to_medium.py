6700645457bf16bb7d01a619701a17e704ec4abe
Bex Tuychiev <bex@ibexprogramming.com>
2021-08-21 10:50:44 +0500

    deploy: Deploy Article 88 to Medium

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/august/',
+jtm.publish('2021/august/6_best_datasets/best_datasets.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title='',
+            title='Tired of Cliché Datasets? Here are 18 Unique Alternatives From All Domains',
             tags=None,
             publish_status='draft',
             notify_followers=False,

3300e697a8a5a926f2c35ebc005225b12b2eb59f
Bex Tuychiev <bex@ibexprogramming.com>
2021-09-02 18:14:11 +0500

    deploy: Deploy article to Medium

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/september/',
+jtm.publish('2021/september/1_lightgbm_intro/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title='',
+            title='How to Beat the Heck Out of XGBoost with LightGBM: Comprehensive Tutorial',
             tags=None,
             publish_status='draft',
             notify_followers=False,

bfd99dfd07bd08b8df6f11da278079b3ea1aa0cc
Bex Tuychiev <bex@ibexprogramming.com>
2021-09-07 21:38:54 +0500

    deploy: Deploy article 90 to Medium

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/september/',
+jtm.publish('2021/september/4_mln_row_datasets/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="How to Work with Million-row Datasets Like a Pro",
             tags=None,
             publish_status='draft',
             notify_followers=False,

6cbb0a0e4d7ebacd3a3e4974bf3d5ffa0a168706
BexTuychiev <bex@ibexprogramming.com>
2021-10-03 21:17:21 +0500

    article: Start article 95

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/september/8_umap_outlier_detection/notebook.ipynb',
+jtm.publish('2021/september/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title="",

e45ba9a62a17b71d808da2ad3782ad93269aa0d6
BexTuychiev <bex@ibexprogramming.com>
2021-10-07 15:23:29 +0500

    article: Article 96, 10 Sklearn Features I sincerely Hope You learn

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/october/1_shap_explainability/notebook.ipynb',
+jtm.publish('2021/october/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="Model Explainability with SHAP: A Guide to Those Who Are Serious About Machine Learning",
+            title="",
             tags=None,
             publish_status='draft',
             notify_followers=False,

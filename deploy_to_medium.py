167b728ea52f24a78f51f72fa30c3109134ef89e
BexTuychiev <bex@ibexprogramming.com>
2021-10-25 16:58:04 +0500

    deploy: Article 101

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/october/10_matplotlib_logrocket/notebook.ipynb',
+jtm.publish('2021/october/9_matplotlib_functions/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="10 Advanced Matplotlib Concepts You Must Know To Create Killer Visuals",
             tags=None,
             publish_status='draft',
             notify_followers=False,

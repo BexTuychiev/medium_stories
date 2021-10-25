7fd1f5a5b5a531e44cc6e2c51709e49bdab31f52
BexTuychiev <bex@ibexprogramming.com>
2021-10-25 22:50:15 +0500

    deploy: Article 102

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/october/',
+jtm.publish('2021/october/3_pandas_mistakes/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="6 Pandas Mistakes That Silently Tell You Are a Rookie",
             tags=None,
             publish_status='draft',
             notify_followers=False,

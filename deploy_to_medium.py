8ff1acedbf5378b420c60b8490e80b0a9e1fec75
BexTuychiev <bex@ibexprogramming.com>
2021-11-03 10:30:00 +0500

    deploy: Article 104

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/october/',
+jtm.publish('2021/november/1_thousands_for_ds/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="How I Earn Thousands For Learning Data Science With No Degree and Experience",
             tags=None,
             publish_status='draft',
             notify_followers=False,

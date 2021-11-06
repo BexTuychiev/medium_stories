5a11ffbe4a74e57a9073ba90864dc8c581e9bb59
BexTuychiev <bex@ibexprogramming.com>
2021-11-06 13:30:37 +0500

    article: Excalidraw

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/november/1_thousands_for_ds/notebook.ipynb',
+jtm.publish('2021/november/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="How I Earn Thousands For Learning Data Science With No Degree and Experience",
+            title="",
             tags=None,
             publish_status='draft',
             notify_followers=False,

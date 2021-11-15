8766d8b2a2db5a1e8377de20aaeebd60a7435df5
BexTuychiev <bex@ibexprogramming.com>
2021-11-15 14:31:17 +0500

    deploy: Matplotlib article for LogRocket

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/november/',
+jtm.publish('2021/november/4_lr_matplotlib/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title="",

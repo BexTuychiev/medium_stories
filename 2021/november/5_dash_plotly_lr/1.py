fa0bcf9fa50739403beb4afbc3db3f9b9914ff81
BexTuychiev <bex@ibexprogramming.com>
2021-11-21 23:04:37 +0500

    section: Dash core components

--- 2021/november/5_dash_plotly_lr/1.py
+++ 2021/november/5_dash_plotly_lr/1.py
@@ -4,14 +4,16 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 
-app = dash.Dash(name="app")
+app = dash.Dash(__name__)
 
-app.layout = html.Div(
-    children=html.H3("Simple Div"),
-    id="sample_div",
-    className="red_div",
-    style={"backgroundColor": "red"},
-)
+app.layout = html.Div([
+    dcc.Slider(
+        min=0,
+        max=9,
+        marks={i: 'Label{}'.format(i) for i in range(10)},
+        value=5,
+    )
+], style={"width": 500})
 
 if __name__ == '__main__':
     app.run_server(debug=True)

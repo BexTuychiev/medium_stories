ddbdd799f413e3d9eaad47530acd7b2350bac481
BexTuychiev <bex@ibexprogramming.com>
2021-11-21 22:46:29 +0500

    section: HTML and CSS components in Dash

--- 2021/november/5_dash_plotly_lr/1.py
+++ 2021/november/5_dash_plotly_lr/1.py
@@ -6,16 +6,12 @@ import pandas as pd
 
 app = dash.Dash(name="app")
 
-# Load dataset using Plotly
-tips = px.data.tips()
-
-fig = px.scatter(tips, x="total_bill", y="tip")  # Create a scatterplot
-
-title = html.H1("Hello Dash!")
-text_div = html.Div("Dash: A web application framework for your data.")
-graph_to_display = dcc.Graph(id="scatter", figure=fig)
-
-app.layout = html.Div(children=[title, text_div, graph_to_display])
+app.layout = html.Div(
+    children=html.H3("Simple Div"),
+    id="sample_div",
+    className="red_div",
+    style={"backgroundColor": "red"},
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)

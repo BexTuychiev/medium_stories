7f380a8b6dbd1e2aa7a4f144210a1e712852dbc5
BexTuychiev <bex@ibexprogramming.com>
2021-12-16 15:47:02 +0500

    article: LogRocket Article, Building Web Scraper in Python

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/december/1_libraries_to_watch_in_2022/notebook.ipynb',
+jtm.publish('2021/december/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="8 Dangerous Data Science Libraries You Must Watch Out in 2022",
+            title="",
             tags=None,
             publish_status='draft',
             notify_followers=False,

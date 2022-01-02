38645ab2c47633c03e855d6cf4e6749e5d664a91
Bex Tuychiev <bex@ibexprogramming.com>
2022-01-02 20:27:26 +0500

    redeploy

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/december/4_python_libraries/notebook.ipynb',
+jtm.publish('2021/december/4_python_builtins/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title="",

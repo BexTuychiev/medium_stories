984cc6c1373f60387298706c40a7bf2c8759c8cd
Bex Tuychiev <bex@ibexprogramming.com>
2022-01-02 21:41:48 +0500

    article: Article 108, How to Get Started on Kaggle in 2022 (Even If You Are Terrified)

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2021/december/4_python_builtins/notebook.ipynb',
+jtm.publish('',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title="",

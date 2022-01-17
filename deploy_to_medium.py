cde5b20fb557f8bb9cbd4352acc286fc5c166fbb
BexTuychiev <bex@ibexprogramming.com>
2022-01-17 17:32:27 +0500

    deploy: Article 109

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2022/1_january/',
+jtm.publish('2022/1_january/2_terality/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="Good-bye Pandas! Meet Terality - Its Evil Twin With Identical Syntax",
             tags=None,
             publish_status='draft',
             notify_followers=False,

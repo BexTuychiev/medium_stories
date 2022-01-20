05e7c8661bc7087504d3ae7ef3cd3e6345095d1a
BexTuychiev <bex@ibexprogramming.com>
2022-01-20 18:52:23 +0500

    deploy: Article 110

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2022/1_january/',
+jtm.publish('2022/1_january/3_aws_s3/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="How to Upload And Download Files From AWS S3 Using Python (2022)",
             tags=None,
             publish_status='draft',
             notify_followers=False,

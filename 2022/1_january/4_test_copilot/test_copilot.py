ff8ad2b9b136dde298903d471468af2b3cc8467c
BexTuychiev <bex@ibexprogramming.com>
2022-01-31 23:30:17 +0500

    section: Visualization

--- 2022/1_january/4_test_copilot/test_copilot.py
+++ 2022/1_january/4_test_copilot/test_copilot.py
@@ -16,3 +16,37 @@ from sklearn.discriminant_analysis import *
 from sklearn.model_selection import *
 from sklearn.pipeline import *
 from sklearn.compose import *
+
+
+import umap
+
+# A function to project data to 2D with UMAP and plot it
+def plot_umap(data, labels, title):
+    # Project the data to 2D
+    reducer = umap.UMAP(n_neighbors=10, min_dist=0.1, n_components=2)
+    embedding = reducer.fit_transform(data)
+
+    # Plot the data
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.scatter(embedding[:, 0], embedding[:, 1], c=labels, cmap='tab10')
+    ax.set_title(title)
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

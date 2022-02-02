b65d79badda66959bbd01ee20329a08129a55701
BexTuychiev <bex@ibexprogramming.com>
2022-02-02 08:22:02 +0500

    deploy: Article 110

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2022/1_january/',
+jtm.publish('2022/1_january/4_test_copilot/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="GitHub Copilot Crushes Data Science And ML Tasks: Ultimate Review",
             tags=None,
             publish_status='draft',
             notify_followers=False,

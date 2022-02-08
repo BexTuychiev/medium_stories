56a268ffca1f3a1a663bbd5ca822ba737f435042
BexTuychiev <bex@ibexprogramming.com>
2022-02-08 16:01:36 +0500

    article: Article 113, DVC fundamentals for data scientists

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2022/2_february/1_julia_for_pythoneers/notebook.ipynb',
+jtm.publish('2022/2_february/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="10-Minute Guide to Julia For Die-Hard Python Lovers",
+            title="",
             tags=None,
             publish_status='draft',
             notify_followers=False,

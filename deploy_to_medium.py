4ed6e6130d37fe5082d261cd23903f7f705dad9d
BexTuychiev <bex@ibexprogramming.com>
2022-02-28 18:04:06 +0500

    deploy: Article 115

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2022/2_february/',
+jtm.publish('2022/2_february/2_dvc_for_data_scientists/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="Open-source ML Project: Improve Pet Adoption With Machine Learning And DagsHub, #1",
             tags=None,
             publish_status='draft',
             notify_followers=False,

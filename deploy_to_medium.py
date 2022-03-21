0d9992386322f62aa56bfbbd9a0f2577980c7522
Bex Tuychiev <bex@ibexprogramming.com>
2022-03-21 15:44:19 +0500

    deploy: Article 116

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2022/2_february/',
+jtm.publish('2022/3_march/2_ml_experimentation/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="Complete Guide to Tracking Your Machine Learning Experiments With MLFlow and DagsHub",
             tags=None,
             publish_status='draft',
             notify_followers=False,

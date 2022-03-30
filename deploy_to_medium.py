7bf0c8c00b23658ab656cbf0c743b2b7724c6f07
BexTuychiev <bex@ibexprogramming.com>
2022-03-30 14:33:22 +0500

    article: Article 117, Python Format Strings

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2022/3_march/2_ml_experimentation/notebook.ipynb',
+jtm.publish('2022/3_march/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="Complete Guide to Tracking Your Machine Learning Experiments With MLFlow and DagsHub",
+            title="",
             tags=None,
             publish_status='draft',
             notify_followers=False,

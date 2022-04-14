96edf7634c028c7670c783be6fb0faf08d22859a
BexTuychiev <bex@ibexprogramming.com>
2022-04-14 08:28:48 +0500

    deploy: Article 117

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2022/3_march/',
+jtm.publish('2022/4_april/1_easiest_model_deployment_pet_part_3/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="The Easiest Way to Deploy Your ML/DL Models in 2022: Streamlit + BentoML + DagsHub",
             tags=None,
             publish_status='draft',
             notify_followers=False,

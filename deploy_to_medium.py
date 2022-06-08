259bff0fcffc279136010a1c8f48ca6a43530722
BexTuychiev <bex@ibexprogramming.com>
2022-06-08 08:34:23 +0500

    article: Article 119, Python TIOBE Index

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2022/6_june/1_spacy_beginner_tutorial/notebook.ipynb',
+jtm.publish('2022/6_june/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title="",

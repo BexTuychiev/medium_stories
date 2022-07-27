30fb8503148a6f8a5030a7a1026d5872187de5e8
BexTuychiev <bex@ibexprogramming.com>
2022-07-27 13:34:27 +0500

    arttıcle: Deployıng Models To AWS Lambda Wıth BentoMLÃ

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2022/7_july/1_week_1/july_3-9.ipynb',
+jtm.publish('2022/7_july/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title="",

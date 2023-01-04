f88290af9648b9b30e33fc1905be5acfea636ecd
Bex Tuychiev <bex@ibexprogramming.com>
2023-01-04 08:20:39 +0500

    article: Article 130, 10 JupyterLab tricks I wish I was Given on My Birthday

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/1_january/1_libraries_to_watch/notebook.ipynb',
+jtm.publish('2023/1_january/',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="6 New Booming Data Science Libraries You Must Learn To Beat The Competition in 2023",
+            title="",
             tags=None,
             publish_status='draft',
             notify_followers=False,

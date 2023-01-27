dba3a9ac7be78cd7f55eff5661558b5f558cd217
Bex Tuychiev <bex@ibexprogramming.com>
2023-01-27 16:05:12 +0500

    deploy: Article 135

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/1_january//notebook.ipynb',
+jtm.publish('2023/1_january/5_outlier_detection_101/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="How to Perform Outlier Detection In Python In Easy Steps For Machine Learning, #1",
             tags=None,
             publish_status='draft',
             notify_followers=False,

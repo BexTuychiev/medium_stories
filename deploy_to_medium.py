ca25abb62f417d88d9d17546dd0d28aa981ba5b4
Bex Tuychiev <bex@ibexprogramming.com>
2023-02-05 20:36:06 +0500

    deploy: Article 138

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/2_february/1_outlier_detection_10/notebook.ipynb',
+jtm.publish('2023/2_february/3_outlier_detection_103/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="How to Perform Multivariate Outlier Detection in Python PyOD For Machine Learning",
             tags=None,
             publish_status='draft',
             notify_followers=False,

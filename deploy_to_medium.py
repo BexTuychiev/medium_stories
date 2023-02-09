f867129cf2eae2a4293af08edfa8d524835722fe
Bex Tuychiev <bex@ibexprogramming.com>
2023-02-09 14:54:05 +0500

    article: 5 Signs You've Become An Advanced Pythonista Without Even Realizing It

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/2_february/3_outlier_detection_103/notebook.ipynb',
+jtm.publish('2023/2_february//notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="How to Perform Multivariate Outlier Detection in Python PyOD For Machine Learning",
+            title="",
             tags=None,
             publish_status='draft',
             notify_followers=False,

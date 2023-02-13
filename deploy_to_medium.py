54884215ce971b53a8fea77e0918781964cf5686
Bex Tuychiev <bex@ibexprogramming.com>
2023-02-13 12:00:02 +0500

    article: Article 139, How to deal with outliers once you find them

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/2_february/4_signs_of_pro/notebook.ipynb',
+jtm.publish('2023/2_february//notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="5 Signs You've Become an Advanced Pythonista Without Even Realizing It",
+            title="",
             tags=None,
             publish_status='draft',
             notify_followers=False,

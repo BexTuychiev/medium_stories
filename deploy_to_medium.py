a845f13830fc10693cab0fdbc8c2b8164d9d5f2a
Bex Tuychiev <bex@ibexprogramming.com>
2023-02-14 07:40:42 +0500

    deploy: Article 139

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/2_february//notebook.ipynb',
+jtm.publish('2023/2_february/5_dealing_with_outliers/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="What to Do With Outliers Once You Find Them? (Hint: You Can't Drop Them)",
             tags=None,
             publish_status='draft',
             notify_followers=False,

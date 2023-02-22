837af7488e2ea64658ab5d3bc6b8dc5c22f86824
Bex Tuychiev <bex@ibexprogramming.com>
2023-02-22 12:15:16 +0500

    deploy: Article draft for Cassie

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/2_february/5_dealing_with_outliers/notebook.ipynb',
+jtm.publish('2023/2_february/6_article_for_cassie/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="What to Do With Outliers Once You Find Them? (Hint: You Can't Drop Them)",
+            title="Inside the Black-box of Machine Learning: Linear Regression",
             tags=None,
             publish_status='draft',
             notify_followers=False,

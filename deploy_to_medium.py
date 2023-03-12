fd776986157043cac3d82dee4e007c71659f423a
Bex Tuychiev <bex@ibexprogramming.com>
2023-03-12 07:22:52 +0500

    section: 8 billion specs of dust

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/2_february/6_article_for_cassie/notebook.ipynb',
+jtm.publish('2023/2_february/7_fractal_art/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="Inside the Black-box of Machine Learning: Linear Regression",
+            title="Create Stunning Fractal Art with Python: A Tutorial For Beginners And Hardcore Math Lovers",
             tags=None,
             publish_status='draft',
             notify_followers=False,
@@ -12,5 +12,5 @@ jtm.publish('2023/2_february/6_article_for_cassie/notebook.ipynb',
             canonical_url=None,
             chrome_path=None,
             save_markdown=False,
-            table_conversion='chrome'
+            table_conversion='matplotlib'
             )

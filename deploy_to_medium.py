ea05fb857859468a94f8db5f7b577b7e880752eb
Bex Tuychiev <bex@ibexprogramming.com>
2023-03-21 19:19:17 +0500

    article: Article 146, Polars vs. Pandas 2.0:

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/3_march/5_data_science_habits/notebook.ipynb',
+jtm.publish('2023/3_march//notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="How to Build Indestructible Data Science Learning Habits Only 5 Minutes a Day",
+            title="",
             tags=None,
             publish_status='draft',
             notify_followers=False,

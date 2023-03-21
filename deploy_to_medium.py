d4d473c37e69aa8b1c51d82fb3382cfb8431f508
Bex Tuychiev <bex@ibexprogramming.com>
2023-03-21 14:49:26 +0500

    deploy: Article 144

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/3_march//notebook.ipynb',
+jtm.publish('2023/3_march/5_data_science_habits/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="How to Build Indestructible Data Science Learning Habits Only 5 Minutes a Day",
             tags=None,
             publish_status='draft',
             notify_followers=False,

3b1e95610613432c3fcfa8f3d4629af30af77680
Bex Tuychiev <bex@ibexprogramming.com>
2023-04-03 14:41:05 +0500

    article: Article 147, Pathlib tricks

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/3_march/7_pandas_to_polars/notebook.ipynb',
+jtm.publish('2023/3_march//notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="7 Easy Steps To Switch From Pandas to Lightning Fast Polars And Never Return",
+            title="",
             tags=None,
             publish_status='draft',
             notify_followers=False,

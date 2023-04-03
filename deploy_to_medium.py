97a47023792848826bbdab4a1ac61483f27b6bf4
Bex Tuychiev <bex@ibexprogramming.com>
2023-04-03 16:53:51 +0500

    deploy: Article 147

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/3_march//notebook.ipynb',
+jtm.publish('2023/4_april/1_pathlib/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="Goodbye os.path: 15 Pathlib Tricks to Quickly Master The File System in Python",
             tags=None,
             publish_status='draft',
             notify_followers=False,

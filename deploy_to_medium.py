8294459aae12dbc3ec9acbe10e5db2dd743b0110
Bex Tuychiev <bex@ibexprogramming.com>
2023-04-16 17:19:56 +0500

    deploy: Article 149"

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/4_april//notebook.ipynb',
+jtm.publish('2023/4_april/3_srs_for_data_science/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="A Proven Method To Remember Everything You Learn in Data Science (No Joke)",
             tags=None,
             publish_status='draft',
             notify_followers=False,

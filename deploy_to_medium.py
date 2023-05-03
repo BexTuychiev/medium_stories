c7455e70f82fca2dccf94d33b92f52c9e9c8451c
Bex Tuychiev <bex@ibexprogramming.com>
2023-05-03 20:53:39 +0500

    section: 1. Tracked, untracked

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/4_april//notebook.ipynb',
+jtm.publish('2023/4_april/4_git_for_data_scientists/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="Git For the Modern Data Scientist: 9 Git Concepts You Can't Ignore",
             tags=None,
             publish_status='draft',
             notify_followers=False,

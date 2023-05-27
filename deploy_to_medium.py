c480661de8302cf634ee5a93ab8b76171e183651
Bex Tuychiev <bex@ibexprogramming.com>
2023-05-28 00:30:07 +0500

    article: Article 154, Sklearn pipelines concepts

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,7 +1,7 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/5_may/3_dask_tutorial/notebook.ipynb',
+jtm.publish('2023/5_may//notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
             title="",

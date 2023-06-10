6a8cfd1f09427481fed7c06952e59d5549985d27
Bex Tuychiev <bex@ibexprogramming.com>
2023-06-11 03:10:11 +0500

    deploy: Article 158

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/6_june//notebook.ipynb',
+jtm.publish('2023/6_june/2_xgb_parameters/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="10 Confusing XGBoost Hyperparameters and How to Tune Them Like a Pro in 2023",
             tags=None,
             publish_status='draft',
             notify_followers=False,

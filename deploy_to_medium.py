1581c4da6eaf8ac629a771088a4e3188a41c7036
Bex Tuychiev <bex@ibexprogramming.com>
2023-06-22 15:25:00 +0500

    deploy: Article 160

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/6_june//notebook.ipynb',
+jtm.publish('2023/6_june/6_future_pythons/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="How to Access Future Python Versions Like 3.12 Before the Masses",
             tags=None,
             publish_status='draft',
             notify_followers=False,

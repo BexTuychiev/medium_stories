cf54b8cf995a40f44e37e292b79a80c632f5e8a3
Bex Tuychiev <bex@ibexprogramming.com>
2023-10-08 17:08:14 +0500

    section: Trying out Chudnovsky

---
+++ 2023/10_october/1_calculating_pi/chudnovsky_pi.py
@@ -0,0 +1,21 @@
+import decimal
+
+def compute_pi(n):
+    decimal.getcontext().prec = n + 1
+    C = 426880 * decimal.Decimal(10005).sqrt()
+    K = 6.
+    M = 1.
+    X = 1
+    L = 13591409
+    S = L
+
+    for i in range(1, n):
+        M = M * (K ** 3 - 16 * K) / ((i + 1) ** 3)
+        L += 545140134
+        X *= -262537412640768000
+        S += decimal.Decimal(M * L) / X
+
+    pi = C / S
+    return pi
+
+compute_pi(1000)

7491e57e29cbc627d77cdb8e927f03de2da2095a
Bex Tuychiev <bex@ibexprogramming.com>
2023-11-18 09:03:12 +0500

    deploy: Anomaly detection article

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/3_march//notebook.ipynb',
+jtm.publish('2023/11_november/1_anomaly_detection/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="",
+            title="Anomaly detection",
             tags=None,
             publish_status='draft',
             notify_followers=False,
@@ -12,5 +12,5 @@ jtm.publish('2023/3_march//notebook.ipynb',
             canonical_url=None,
             chrome_path=None,
             save_markdown=False,
-            table_conversion='chrome'
+            table_conversion='matplotlib'
             )

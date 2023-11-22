b24479306f7312bfd31fc3309d995aa3ec07beb4
Bex Tuychiev <bex@ibexprogramming.com>
2023-11-22 07:19:18 +0500

    deploy: Skewness and kurtosis article

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/11_november/1_anomaly_detection/notebook.ipynb',
+jtm.publish('2023/11_november/2_skew_kurtosis/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="Anomaly detection",
+            title="Skew and kurtosis",
             tags=None,
             publish_status='draft',
             notify_followers=False,

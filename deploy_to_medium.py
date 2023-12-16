7c525f2669e5b814e28e2b15cd9fdaa039ccb666
Bex Tuychiev <bex@ibexprogramming.com>
2023-12-16 20:25:40 +0500

    deploy: Gradient boosting article

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,10 +1,10 @@
 import jupyter_to_medium as jtm
 import os
 
-jtm.publish('2023/11_november/3_mysql_docker/notebook.ipynb',
+jtm.publish('2023/12_december/1_gradient_boosting/notebook.ipynb',
             integration_token=os.environ['TOKEN'],
             pub_name=None,
-            title="MySQL + Docker",
+            title="A Guide to the Gradient Boosting Algorithm",
             tags=None,
             publish_status='draft',
             notify_followers=False,

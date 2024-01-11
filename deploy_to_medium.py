87eeee04a086233d89fb495ee18779f1bb3a2ff8
Bex Tuychiev <bex@ibexprogramming.com>
2024-01-11 10:43:19 +0500

    deploy: Introduction to dbt

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -5,10 +5,10 @@ import toml
 with open('config.toml', 'r') as file:
     data = toml.load(file)
 
-jtm.publish('2023/12_december//notebook.ipynb',
+jtm.publish('2023/12_december/3_intro_dbt/notebook.ipynb',
             integration_token=data['codes']['TOKEN'],
             pub_name=None,
-            title="",
+            title="Introduction to dbt: 10 Must-Know Concepts For Data Engineers",
             tags=None,
             publish_status='draft',
             notify_followers=False,

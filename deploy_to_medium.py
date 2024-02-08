9b1801d80379b823424bb0eac369867ef375dc40
Bex Tuychiev <bex@ibexprogramming.com>
2024-02-08 12:38:56 +0500

    deploy: Salesforce xgen 7 article

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -5,10 +5,10 @@ import toml
 with open('config.toml', 'r') as file:
     data = toml.load(file)
 
-jtm.publish('2024/1_january//notebook.ipynb',
+jtm.publish('2024/2_february/2_xgen/notebook.ipynb',
             integration_token=data['codes']['TOKEN'],
             pub_name=None,
-            title="",
+            title="Salesforce XGen-7B: A Step-by-step tutorial on Using And Fine-Tuning XGen-7B",
             tags=None,
             publish_status='draft',
             notify_followers=False,

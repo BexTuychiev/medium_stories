2d2a5e0926bec077d3774a7dbf12e47ebf3821fa
Bex Tuychiev <bex@ibexprogramming.com>
2024-02-26 15:54:30 +0500

    article: Datacamp dataclasses article

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -5,10 +5,10 @@ import toml
 with open('config.toml', 'r') as file:
     data = toml.load(file)
 
-jtm.publish('2024/2_february/3_ag_data_clean/notebook.ipynb',
+jtm.publish('2024/2_february//notebook.ipynb',
             integration_token=data['codes']['TOKEN'],
             pub_name=None,
-            title="How to Explore and Clean Sensitive Data You Can't Even See With Antigranular",
+            title="",
             tags=None,
             publish_status='draft',
             notify_followers=False,

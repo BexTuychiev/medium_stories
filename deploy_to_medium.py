f0edf384e8099d8a5ce1d11b71891047bedc1e6f
Bex Tuychiev <bex@ibexprogramming.com>
2024-02-28 15:26:35 +0500

    deploy: Data classes article for datacamp

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -5,10 +5,10 @@ import toml
 with open('config.toml', 'r') as file:
     data = toml.load(file)
 
-jtm.publish('2024/2_february//notebook.ipynb',
+jtm.publish('2024/2_february/4_dataclasses/notebook.ipynb',
             integration_token=data['codes']['TOKEN'],
             pub_name=None,
-            title="",
+            title="Learn Data Classes in Python: Comprehensive Tutorial",
             tags=None,
             publish_status='draft',
             notify_followers=False,

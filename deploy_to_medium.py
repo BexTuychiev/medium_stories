dd2bf398b754c85f52de8139bbb18c8dce0b3fcd
Bex Tuychiev <bex@ibexprogramming.com>
2024-04-14 21:32:03 +0500

    article: Encapsulation in Python

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -5,10 +5,10 @@ import toml
 with open('config.toml', 'r') as file:
     data = toml.load(file)
 
-jtm.publish('2024/4_april/4_env_variables/notebook.ipynb',
+jtm.publish('2024/4_april//notebook.ipynb',
             integration_token=data['codes']['TOKEN'],
             pub_name=None,
-            title="Everything You Need to Know About Python Environment Variables",
+            title="",
             tags=None,
             publish_status='draft',
             notify_followers=False,

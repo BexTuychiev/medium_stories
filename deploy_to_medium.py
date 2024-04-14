d02d39c07e8a08f5a563a503f5a1111cb81cbe71
Bex Tuychiev <bex@ibexprogramming.com>
2024-04-14 15:07:50 +0500

    deploy: Snowflake snowpark article

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -5,10 +5,10 @@ import toml
 with open('config.toml', 'r') as file:
     data = toml.load(file)
 
-jtm.publish('2024/4_april//notebook.ipynb',
+jtm.publish('2024/4_april/3_snowflake_snowpark/notebook.ipynb',
             integration_token=data['codes']['TOKEN'],
             pub_name=None,
-            title="",
+            title="A Comprehensive Introduction to Snowflake Snowpark",
             tags=None,
             publish_status='draft',
             notify_followers=False,

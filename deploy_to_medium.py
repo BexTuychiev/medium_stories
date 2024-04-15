13f8eff550d69d92cd55f08f4ee2760b733cebac
Bex Tuychiev <bex@ibexprogramming.com>
2024-04-15 17:47:36 +0500

    article: OCR in Python

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -5,10 +5,10 @@ import toml
 with open('config.toml', 'r') as file:
     data = toml.load(file)
 
-jtm.publish('2024/4_april/5_encapsulation/notebook.ipynb',
+jtm.publish('2024/4_april//notebook.ipynb',
             integration_token=data['codes']['TOKEN'],
             pub_name=None,
-            title="Comprehensive Tutorial on Encapsulation in Python Object-oriented Programming",
+            title="",
             tags=None,
             publish_status='draft',
             notify_followers=False,

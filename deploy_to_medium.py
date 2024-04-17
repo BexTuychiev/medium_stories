abc515ad305f96ac544fc78e7cc0398827322532
Bex Tuychiev <bex@ibexprogramming.com>
2024-04-17 14:26:42 +0500

    article: Data visualization in Antigranular

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -5,10 +5,10 @@ import toml
 with open('config.toml', 'r') as file:
     data = toml.load(file)
 
-jtm.publish('2024/4_april/6_ocr/notebook.ipynb',
+jtm.publish('2024/4_april//notebook.ipynb',
             integration_token=data['codes']['TOKEN'],
             pub_name=None,
-            title="A Comprehensive Tutorial on Optical Character Recognition (OCR) in Python With Pytesseract",
+            title="",
             tags=None,
             publish_status='draft',
             notify_followers=False,

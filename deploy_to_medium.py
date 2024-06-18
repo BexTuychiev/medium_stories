bf653a90c6c34f75211ad788d1b4acbe7f9bcce7
Bex Tuychiev <bex@ibexprogramming.com>
2024-06-18 09:59:53 +0500

    article: Introduction to Vizly

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -2,19 +2,20 @@ import jupyter_to_medium as jtm
 import os
 import toml
 
-with open('config.toml', 'r') as file:
+with open("config.toml", "r") as file:
     data = toml.load(file)
 
-jtm.publish('2024/6_june//notebook.ipynb',
-            integration_token=data['codes']['TOKEN'],
-            pub_name=None,
-            title="",
-            tags=None,
-            publish_status='draft',
-            notify_followers=False,
-            license='all-rights-reserved',
-            canonical_url=None,
-            chrome_path=None,
-            save_markdown=False,
-            table_conversion='matplotlib'
-            )
+jtm.publish(
+    "2024/6_june//notebook.ipynb",
+    integration_token=data["codes"]["TOKEN"],
+    pub_name=None,
+    title="",
+    tags=None,
+    publish_status="draft",
+    notify_followers=False,
+    license="all-rights-reserved",
+    canonical_url=None,
+    chrome_path=None,
+    save_markdown=False,
+    table_conversion="matplotlib",
+)

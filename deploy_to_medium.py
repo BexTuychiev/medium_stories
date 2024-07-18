d20bb98e9a22a9733e0d216ca2fba4db988526bc
Bex Tuychiev <bex@ibexprogramming.com>
2024-07-18 18:14:17 +0500

    article: List of computer vision projects

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -6,10 +6,10 @@ with open("config.toml", "r") as file:
     data = toml.load(file)
 
 jtm.publish(
-    "2024/7_july/4_sgd/notebook.ipynb",
+    "2024/7_july//notebook.ipynb",
     integration_token=data["codes"]["TOKEN"],
     pub_name=None,
-    title="SGD Explained",
+    title="",
     tags=None,
     publish_status="draft",
     notify_followers=False,

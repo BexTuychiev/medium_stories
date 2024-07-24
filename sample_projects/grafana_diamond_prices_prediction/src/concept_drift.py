67379bc42e446c8359dc8b628a07f279d4089959
Bex Tuychiev <bex@ibexprogramming.com>
2024-07-24 17:15:27 +0500

    feature: Simplify the pre-processing process

--- sample_projects/grafana_diamond_prices_prediction/src/concept_drift.py
+++ sample_projects/grafana_diamond_prices_prediction/src/concept_drift.py
@@ -4,10 +4,10 @@ import numpy as np
 
 
 def detect_concept_drift(
-    model, X_reference, y_reference, X_current, y_current, threshold=0.1
+    model_pipeline, X_reference, y_reference, X_current, y_current, threshold=0.1
 ):
-    y_pred_reference = model.predict(X_reference)
-    y_pred_current = model.predict(X_current)
+    y_pred_reference = model_pipeline.predict(X_reference)
+    y_pred_current = model_pipeline.predict(X_current)
 
     mse_reference = mean_squared_error(y_reference, y_pred_reference)
     mse_current = mean_squared_error(y_current, y_pred_current)

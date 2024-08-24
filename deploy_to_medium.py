f9fb572784b4115a3b881ccc5e8795675e852332
Bex Tuychiev <bex@ibexprogramming.com>
2024-08-24 13:53:14 +0500

    section: Introduction, LangChain rag

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -6,7 +6,7 @@ with open("config.toml", "r") as file:
     data = toml.load(file)
 
 jtm.publish(
-    "2024/8_august//notebook.ipynb",
+    "2024/8_august/1_adam_optimizer/notebook.ipynb",
     integration_token=data["codes"]["TOKEN"],
     pub_name=None,
     title="",
@@ -19,5 +19,3 @@ jtm.publish(
     save_markdown=False,
     table_conversion="matplotlib",
 )
-
-

b6351256c5727eb38486cd59f9087790b19d5a94
Bex Tuychiev <bex@ibexprogramming.com>
2024-08-28 22:12:56 +0500

    article: Sarsa reinforcement learning algorithm in python

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -1,15 +1,14 @@
 import jupyter_to_medium as jtm
-import os
 import toml
 
 with open("config.toml", "r") as file:
     data = toml.load(file)
 
 jtm.publish(
-    "2024/8_august/2_rag_langchain/notebook.ipynb",
+    "2024/8_august//notebook.ipynb",
     integration_token=data["codes"]["TOKEN"],
     pub_name=None,
-    title="Building a RAG system with LangChain",
+    title="",
     tags=None,
     publish_status="draft",
     notify_followers=False,

e5e41c7781914b1c53461670224cd97e4baa6ff8
Bex Tuychiev <bex@ibexprogramming.com>
2024-09-04 14:31:24 +0500

    article: RMSProp

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -5,7 +5,7 @@ with open("config.toml", "r") as file:
     data = toml.load(file)
 
 jtm.publish(
-    "2024/9_september/1_structured_outputs_openai/notebook.ipynb",
+    "2024/9_september//notebook.ipynb",
     integration_token=data["codes"]["TOKEN"],
     pub_name=None,
     title="",

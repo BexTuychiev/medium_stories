cef9057b1465dbeceeee469b92c1e39c84bbd624
Bex Tuychiev <bex@ibexprogramming.com>
2024-09-08 15:18:06 +0500

    deploy: RMSprop

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -5,7 +5,7 @@ with open("config.toml", "r") as file:
     data = toml.load(file)
 
 jtm.publish(
-    "2024/9_september//notebook.ipynb",
+    "2024/9_september/2_rmsprop/notebook.ipynb",
     integration_token=data["codes"]["TOKEN"],
     pub_name=None,
     title="",

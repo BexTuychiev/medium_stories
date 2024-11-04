3803cb0b81836bd72d0d96b75fe55d85c737cf62
BexTuychiev <bex@ibexprogramming.com>
2024-11-04 15:22:35 +0500

    section: Configuring poetry

--- deploy_to_medium.py
+++ deploy_to_medium.py
@@ -5,7 +5,7 @@ with open("config.toml", "r") as file:
     data = toml.load(file)
 
 jtm.publish(
-    "2024/10_october//notebook.ipynb",
+    "2024/11_november/1_oop_java/notebook.ipynb",
     integration_token=data["codes"]["TOKEN"],
     pub_name=None,
     title="",
